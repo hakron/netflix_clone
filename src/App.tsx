c2ad8dda3913fff826624ff4e93dfd230801bb44
hakron88 <hakronardo@gmail.com>
2020-09-22 17:55:52 +0200

    create the row to display the movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import Row from "./components/Row/Row";
+import requests from "./request";
+import './App.css'
 
-function App() {
+const App: React.FunctionComponent = (props) => {
   return (
     <div className="App">
-      <p>Netflix</p>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
-}
+};
 
 export default App;
