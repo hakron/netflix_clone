4da5bac137114416eaea452377e20f3ef1c2bb3d
DESKTOP-B99HTT4\enriq <enriquealonso608@hotmail.com>
2020-10-30 17:21:43 +0100

    move the movie type to own file

---
+++ src/types.tsx
@@ -0,0 +1,16 @@
+export interface Movie {
+  backdrop_path: string;
+  first_air_date: string;
+  title: string;
+  genre_ids: string[];
+  id: number;
+  name: string;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+}
